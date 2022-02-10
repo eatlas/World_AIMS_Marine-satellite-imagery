0ba9054c1ca24b61874fe65ad2c5286d2e08f0ed
eric.lawrey <eric.lawrey@gmail.com>
2022-02-10 04:59:06 +0000

    Updated s2Utils.js (added 147 lines).

--- s2Utils.js
+++ s2Utils.js
@@ -1790,3 +1790,150 @@ exports.createSelectSentinel2ImagesApp = function(tileID, startDate, endDate, cl
   prevButton.onClick(function(button) { setImage(button, -1); });
   nextButton.onClick(function(button) { setImage(button, 1); });
 };
+
+
+
+
+
+
+
+
+
+
+// Use this app to browse through a specific set of Sentinel 2
+// images. This can be used to fine tune the selection of images obtained
+// using the createSelectSentinel2ImagesApp. This saves having to
+// step through all the non relevant images.
+// @param {[string]} imageIds - array of Sentinel 2 image IDs. 
+//    imageIds = ["COPERNICUS/S2/20160824T015622_20160824T015622_T51LXF",
+//      "COPERNICUS/S2/20160913T015622_20160913T015617_T51LXF",
+//      "COPERNICUS/S2/20170730T015701_20170730T015834_T51LXF"]
+exports.viewSelectedSentinel2ImagesApp = function(imageIds) {
+  
+  // Find the bounds of all the tiles that the images are in. Restrict the search
+  // to the tropics where reefs are found. This is to speed up the code.
+  var tilesGeometry = exports.get_s2_tiles_geometry(imageIds, ee.Geometry.BBox(-180, -33, 180, 33));
+  
+  var s2_cloud_collection = exports.get_s2_cloud_collection(imageIds, tilesGeometry);
+  
+  // Zoom to our tile of interest.
+  Map.centerObject(tilesGeometry, 9);
+  
+  // Adjust the collection of images
+  var collection = s2_cloud_collection;
+    //.map(utils.removeSunGlint);
+  
+  var listOfImage = collection.toList(collection.size());
+  
+  // Sets up next and previous buttons used to navigate through previews of the
+  // images in the collection.
+  var prevButton = new ui.Button('Previous', null, true, {margin: '0 auto 0 0'});
+  var nextButton = new ui.Button('Next', null, true, {margin: '0 0 0 auto'});
+  var buttonPanel = new ui.Panel(
+      [prevButton, nextButton],
+      ui.Panel.Layout.Flow('horizontal'));
+      
+  // Build the display panel
+  var introPanel = ui.Panel([
+    ui.Label({
+      value: 'Browse images',
+      style: {fontWeight: 'bold', fontSize: '24px', margin: '10px 5px'}
+    }),
+    //ui.Label('')
+  ]);
+  
+  // Setup the user interface
+  var progressLabel = ui.Label({style: {margin: '2px 0'}});
+  var idLabel = ui.Label({style: {margin: '2px 0'}});
+  var mainPanel = ui.Panel({
+    widgets: [introPanel, idLabel, progressLabel, buttonPanel,],
+    style: {position: 'bottom-left', width: '340px'}
+  });
+  Map.add(mainPanel);
+  
+  
+  var selectedIndex = 0;
+  var collectionLength = 0;
+  // Get the total number of images asynchronously, so we know how far to step.
+  // This async process because we want the value on the client but the size
+  // is a server side value.
+  listOfImage.size().evaluate(function(length) {
+    collectionLength = length;
+    updateUI();
+  });
+  
+  
+  var updateUI = function() {
+  
+    progressLabel.setValue('Image: '+(selectedIndex+1)+' of '+(collectionLength));
+  
+  
+    var image = ee.Image(listOfImage.get(selectedIndex));
+    // Don't perform the cloud removal because this is computationally
+    // expensive and significantly slows down the calculation of the images.
+    var visParams = {'min': 0, 'max': 1, 'gamma': 1};
+    var composite = exports.removeSunGlint(image)
+      .rename(['B1','B2','B3','B4','B5','B6','B7','B8',
+        'B8A','B9','B10','B11','B12','QA10','QA20','QA60']);
+    var projection = composite.select('B1').projection();
+    var includeCloudmask = false;
+    
+    Map.layers().reset();
+    var trueColour_composite = exports.bake_s2_colour_grading(composite, 'TrueColour', includeCloudmask);
+    Map.addLayer(trueColour_composite, visParams, 'Sentinel-2 True Colour',false);
+    
+    var deepMarine_composite = exports.bake_s2_colour_grading(composite, 'DeepFalse', includeCloudmask);
+    Map.addLayer(deepMarine_composite, visParams, 'Sentinel-2 Deep False',true);
+  
+    var reefTop_composite = exports.bake_s2_colour_grading(composite, 'ReefTop', includeCloudmask);
+    Map.addLayer(reefTop_composite, visParams, 'Sentinel-2 ReefTop',false);
+  
+    var slope_composite = exports.bake_s2_colour_grading(composite, 'Slope', includeCloudmask, projection);
+    Map.addLayer(slope_composite, visParams, 'Sentinel-2 Slope',false);
+    
+    //var deepMarine2_composite = utils.bake_s2_colour_grading(composite, 'TrueColourA', includeCloudmask);
+    //print(deepMarine_composite);
+    //print(deepMarine2_composite);
+    
+    //Map.addLayer(deepMarine2_composite, visParams, 'Sentinel-2 Deep Marine2',true);
+    
+    var shallow_composite = exports.bake_s2_colour_grading(composite, 'Shallow', includeCloudmask);
+    Map.addLayer(shallow_composite, visParams, 'Sentinel-2 Shallow',false);
+  
+    Map.addLayer(deepMarine_composite.select('vis-blue'), visParams, 'Sentinel-2 Deep Marine vis-blue',false);
+    Map.addLayer(deepMarine_composite.select('vis-green'), visParams, 'Sentinel-2 Deep Marine vis-green',false);
+    Map.addLayer(deepMarine_composite.select('vis-red'), visParams, 'Sentinel-2 Deep Marine vis-red',false);
+    Map.addLayer(composite.select("B1"), {'min': 1100, 'max': 1500, 'gamma': 2}, 'Sentinel-2 B1 after glint removal',false);
+    Map.addLayer(composite.select("B2"), {'min': 650, 'max': 1500, 'gamma': 2}, 'Sentinel-2 B2 after glint removal',false);
+    Map.addLayer(composite.select("B4"), {'min': 0, 'max': 1500, 'gamma': 2}, 'Sentinel-2 B4 after glint removal',false);
+    Map.addLayer(composite.select("B5"), {'min': 0, 'max': 1500, 'gamma': 2}, 'Sentinel-2 B5 raw',false);
+    Map.addLayer(composite.select("B8"), {'min': 0, 'max': 1500, 'gamma': 2}, 'Sentinel-2 B8 raw',false);
+    Map.addLayer(composite.select("B11"), {'min': 0, 'max': 1500, 'gamma': 2}, 'Sentinel-2 B11 raw',false);
+    Map.addLayer(image, {
+        bands: ['B4', 'B3', 'B2'],
+        min: [130, 200, 500],
+        max: [1700, 1900, 2000],
+        gamma: [2, 2, 2]
+      }, 'Sentinel-2 Raw',false);
+  
+    
+  
+    nextButton.setDisabled(selectedIndex >= collectionLength - 1);
+    prevButton.setDisabled(selectedIndex <= 0);
+  };
+  
+  // Gets the index of the next/previous image in the collection and sets the
+  // thumbnail to that image.  Disables the appropriate button when we hit an end.
+  var setImage = function(button, increment) {
+    if (button.getDisabled()) return;
+    //setImageByIndex(selectedIndex += increment);
+    selectedIndex += increment;
+    updateUI();
+  };
+  
+  // Set up the next and previous buttons.
+  prevButton.onClick(function(button) { setImage(button, -1); });
+  nextButton.onClick(function(button) { setImage(button, 1); });
+  
+  //updateUI();
+};
