6532c73f013f5b1b7e5a4fbdf89e7c34a7755b89
eric.lawrey <eric.lawrey@gmail.com>
2022-02-10 04:12:53 +0000

    Updated s2-composites/Eastern-Micronesia.js (added 21 lines, deleted 1 line).

---
+++ s2-composites/Eastern-Micronesia.js
@@ -0,0 +1,21 @@
+//04NFH Kitibati, Kiritimati
+// Not enough images
+// 26 of 26 images searched
+
+utils.s2_composite_display_and_export(
+  [
+    //Good images
+    "COPERNICUS/S2/20200626T210451_20200626T210448_T04NFH",
+    "COPERNICUS/S2/20200517T210451_20200517T210448_T04NFH",
+    // okay
+    "COPERNICUS/S2/20170707T210439_20170707T210442_T04NFH"
+  ],
+  false, false, REF1_OPTIONS);
+
+
+utils.s2_composite_display_and_export(
+  [
+    // maybe images          
+    "COPERNICUS/S2/20190702T210451_20190702T210448_T04NFH"
+  ],
+  false, false, REF2_OPTIONS);
