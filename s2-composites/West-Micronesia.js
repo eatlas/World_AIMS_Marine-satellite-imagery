386b9d31c0d5a0a620f983810ae28cb4d7e252a9
eric.lawrey <eric.lawrey@gmail.com>
2022-02-10 04:15:19 +0000

    Updated s2-composites/West-Micronesia.js (added 21 lines, deleted 1 line).

---
+++ s2-composites/West-Micronesia.js
@@ -0,0 +1,21 @@
+ // 53NMJ Palau
+// Not enough images
+// 
+
+utils.s2_composite_display_and_export(
+  [
+    //good images
+    "COPERNICUS/S2/20211026T013459_20211026T013454_T53NMJ",
+    //okay
+    "COPERNICUS/S2/20200911T013459_20200911T013455_T53NMJ" // sunglint on the edge
+  ],
+  false, false, REF1_OPTIONS);
+
+
+utils.s2_composite_display_and_export(
+  [
+    // maybe images          
+    "COPERNICUS/S2/20180420T013451_20180420T013454_T53NMJ",
+    "COPERNICUS/S2/20180624T013449_20180624T013451_T53NMJ"
+  ],
+  false, false, REF2_OPTIONS);
