7a7611083efd509af782c39a248d817dc0db5649
eric.lawrey <eric.lawrey@gmail.com>
2022-03-13 07:02:11 +0000

    Updated src/gee/gee-debug/satellite-bathymetry.js (added 15 lines, deleted 3 lines).

--- src/gee/gee-debug/satellite-bathymetry.js
+++ src/gee/gee-debug/satellite-bathymetry.js
@@ -39,21 +39,33 @@ Map.addLayer(composite, {'bands':['B4', 'B3', 'B2'], min: 0, max:1400}, 'Sentine
 
 var B3 = composite.select('B3');
 var B2 = composite.select('B2');
+var B8 = composite.select('B8');
 
 Map.centerObject(composite.geometry(), 10);
 
 // Dark or deep
 // Create an image that gets brighter as we approach the dark threshold caused by seagrass.
+// This works because we are focusing on areas shallow than 15 m and areas that have a
+// dark substrate from seagrass are darker in B2 at all depths than sand even at -15 m.
 // Normalise the brightness to 0 - 1, where 1 corresponds to open water or dark seagrass.
 var darkOrDeepImg = ee.Image(900).subtract(B2).max(ee.Image(0)).divide(160);
-Map.addLayer(darkOrDeepImg, {min: 0, max:1}, 'Dark or deep');
+//Map.addLayer(darkOrDeepImg, {min: 0, max:1}, 'Dark or deep');
 
 // Deep. Find areas deeper than can be detected with green (~15 m)
 // Normalise the brightness to 0 - 1, where 1 corresponds to open water.
 var deep = ee.Image(400).subtract(B3).max(ee.Image(0)).divide(60);
-Map.addLayer(deep, {min: 0, max:1}, 'Deep');
+//Map.addLayer(deep, {min: 0, max:1}, 'Deep');
 
 // Now combine to focus on seagrass areas
 var darkImg = darkOrDeepImg.subtract(deep);
 
 Map.addLayer(darkImg, {min: 0, max:1}, 'Dark');
+
+
+// We need to exclude dry areas as we don't which to apply any compensation to land areas.
+// Use B8 channel as a simple land mask.
+// Water areas with sunglint can reflect up to 400. So we use this as a threshold.
+// Make sure water areas are black. Scale to approx 0 - 1. Land areas are 400 - 2500
+var water = ee.Image(400).subtract(B8).divide(1000); 
+
+Map.addLayer(B8, {min: 0, max:1}, 'Water');
