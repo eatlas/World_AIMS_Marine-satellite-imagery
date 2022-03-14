cf2731074d632ad71bafbc29f0c569b487f25429
eric.lawrey <eric.lawrey@gmail.com>
2022-03-14 07:00:32 +0000

    Updated src/gee/gee-debug/satellite-bathymetry.js (added 6 lines, deleted 1 line).

--- src/gee/gee-debug/satellite-bathymetry.js
+++ src/gee/gee-debug/satellite-bathymetry.js
@@ -62,9 +62,14 @@ Map.addLayer(B4, {min: 0, max:400}, 'B4');
 // shallow and dark areas. Though possibly at the linearity of the response with depth.
 var B4_BLACK_POINT = 190;
 var B4_NORM_SCALAR = 400;
-var B4_SCALAR = 1;
+var B4_SCALAR = 0.5;
 var b4scale = ee.Image(1).subtract(B4.subtract(B4_BLACK_POINT).max(0).divide(B4_NORM_SCALAR).min(1).multiply(B4_SCALAR));
 Map.addLayer(b4scale, {min: 0, max:1}, 'B4 scale');
+
+
+// Scaled
+var depthB4B3B2 = b4scale.multiply(depthB3B2)
+Map.addLayer(depthB4B3B2, {min: -15, max:1}, 'depthB4B3B2');
 
 // =====================================================================
 //                Piece wise depth model
