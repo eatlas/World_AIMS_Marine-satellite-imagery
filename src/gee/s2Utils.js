abf7d125f94d97fca68fe72bddb47fbec24b7965
eric.lawrey <eric.lawrey@gmail.com>
2022-03-14 10:39:06 +0000

    Updated src/gee/s2Utils.js (added 1 line, deleted 1 line).

--- src/gee/s2Utils.js
+++ src/gee/s2Utils.js
@@ -1619,7 +1619,7 @@ exports.bake_s2_colour_grading = function(img, colourGradeStyle, processCloudMas
     
     // Perform spatial filtering to reduce the noise. This will make the depth estimates between for creating contours.
     //compositeContrast = depthB3B2; //.focal_mean({kernel: ee.Kernel.circle({radius: 20, units: 'meters'}), iterations: 2});
-    compositeContrast = scaled_img.select('B2');
+    compositeContrast = scaled_img.select('B3').log();
     //compositeContrast = exports.contrastEnhance(depthB3B2,-25.5,0, 1);
     
   } else {
