4e928eb6b6facffe8a70bef334c4943f96e6bdda
eric.lawrey <eric.lawrey@gmail.com>
2022-03-15 04:45:10 +0000

    Updated src/gee/s2-composites/GBR-Torres-Strait.js (added 1 line, deleted 1 line).

--- src/gee/s2-composites/GBR-Torres-Strait.js
+++ src/gee/s2-composites/GBR-Torres-Strait.js
@@ -4,7 +4,7 @@ var s2Utils = require('users/ericlawrey/World_AIMS_Marine-satellite-imagery:src/
 var REF1_OPTIONS = {
   //colourGrades: ['DeepFalse','TrueColour','ReefTop','Shallow','Slope'],
   //exportScale: [10, 10, 10, 10, 30],
-  colourGrades: ['Depth'],
+  colourGrades: ['Depth10m'],
   exportScale: [10],
   exportBasename: 'World_AIMS_Marine-satellite-imagery_R1',
   exportFolder: 'EarthEngine/World_AIMS_Marine-satellite-imagery/GBR-Torres-Strait',
