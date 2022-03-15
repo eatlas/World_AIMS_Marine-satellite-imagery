dee53f74d2c3ca979b4eb0f1b349cba6e1fc3714
eric.lawrey <eric.lawrey@gmail.com>
2022-03-15 05:03:19 +0000

    Updated src/gee/s2-composites/Coral-Sea.js (added 2 lines, deleted 2 lines).

--- src/gee/s2-composites/Coral-Sea.js
+++ src/gee/s2-composites/Coral-Sea.js
@@ -12,8 +12,8 @@ var s2Utils = require('users/ericlawrey/World_AIMS_Marine-satellite-imagery:src/
 var REF1_OPTIONS = {
   //colourGrades: ['DeepFalse','TrueColour','ReefTop','Shallow','Slope'],
   //exportScale: [10, 10, 10, 10, 30],
-  colourGrades: ['Depth10m'],
-  exportScale: [20],
+  colourGrades: ['ReefTop','Depth5m'],
+  exportScale: [20, 20],
   exportBasename: 'World_AIMS_Marine-satellite-imagery_R1',
   exportFolder: 'EarthEngine/World_AIMS_Marine-satellite-imagery/Coral-Sea',
   
