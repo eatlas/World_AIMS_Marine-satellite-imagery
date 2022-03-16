fcc310e3912da38ee8c8d138e7df445232c9209d
eric.lawrey <eric.lawrey@gmail.com>
2022-03-16 10:37:29 +0000

    Updated src/gee/s2Utils.js (added 1 line, deleted 1 line).

--- src/gee/s2Utils.js
+++ src/gee/s2Utils.js
@@ -1405,7 +1405,7 @@ exports.bake_s2_colour_grading = function(img, colourGradeStyle, processCloudMas
     // not to pick up deeper areas. A comparison showed that it was far more accurate than the
     // B3/B2 depth estimate. B5 does not have sunglint correction, however the threshold we are
     // using is above typical effects of sunglint.
-    compositeContrast = scaled_img.select('B5').gt(0.06);
+    compositeContrast = scaled_img.select('B5').gt(0.028);
 
   } else if (colourGradeStyle === 'ReefTop') {
     //B4contrast = exports.contrastEnhance(scaled_img.select('B4'),0.02,0.021, 1);
