856dc7e7170f616e332427c223add8a49c6c225d
eric.lawrey <eric.lawrey@gmail.com>
2022-03-16 08:10:51 +0000

    Updated src/gee/dev/B5sunglint.js (added 12 lines, deleted 2 lines).

--- src/gee/dev/B5sunglint.js
+++ src/gee/dev/B5sunglint.js
@@ -1,8 +1,18 @@
 //var img = ee.Image("COPERNICUS/S2/20190220T234701_20190220T234701_T56KQB");
-var img = ee.Image("COPERNICUS/S2/20190918T002051_20190918T002054_T55KFB");
+
+// Hearld reef
+// This shows breaking waves.
+var img = ee.Image("COPERNICUS/S2/20200930T002709_20200930T002710_T55KFB");
 
 var B5 = img.select('B5');
 var B11 = img.select('B11');
+
+// B11 appears to be less sensitive to surface water spray then B5 and so breaking waves are 
+// dimmer in B11. This is good as it means that B5-B11 doesn't result in B11 removing the
+// breaking waves from B5. 
+// The sensitivity of B11 and B5 to clouds is similar and so B5-B11 removes much of the clouds
+// from the image. The edges of clouds are reasonably corrected and solid cloud areas turn black.
+
 // Zoom to our tile of interest.
 Map.centerObject(img.geometry(), 10);
 
