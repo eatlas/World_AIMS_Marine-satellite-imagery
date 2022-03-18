d3e70897cd06476af12244f1e30f18ce8e654a14
eric.lawrey <eric.lawrey@gmail.com>
2022-03-18 04:06:31 +0000

    Updated src/gee/debug/export-empty-geojson.js (added 7 lines, deleted 1 line).

--- src/gee/debug/export-empty-geojson.js
+++ src/gee/debug/export-empty-geojson.js
@@ -1,4 +1,10 @@
-  
+
+// This script test the behaviour of converting an image to vector when there is nothing
+// to digitise. I have found that when export as Shapefile the Task fails indicating that
+// it can't export an empty set to Shapefile. I am see that with GeoJSON format I am getting
+// broken files made that consist of just two brackets "]}". This debug script is to test
+// if this problem is caused by exporting empty vectors.
+
 // Get a real image to ensure the geopatial coordinates are valid
 var img = ee.Image("COPERNICUS/S2/20200909T005709_20200909T005710_T54LXP");
 // Make sure there is nothing to generate a vector from.
