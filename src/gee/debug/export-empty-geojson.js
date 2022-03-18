a04590d6df2ccd0b7880ba110c0b4a7fe5b92ba0
eric.lawrey <eric.lawrey@gmail.com>
2022-03-18 04:03:24 +0000

    Updated src/gee/debug/export-empty-geojson.js (added 24 lines, deleted 26 lines).

--- src/gee/debug/export-empty-geojson.js
+++ src/gee/debug/export-empty-geojson.js
@@ -1,27 +1,25 @@
-
-
-  // Convert the image to vectors.
-  var vector = imgContour.reduceToVectors({
-    geometry: geometry,
-    crs: imgContour.projection(),
-    scale: scale,
-    geometryType: 'polygon',
-    eightConnected: false,
-    labelProperty: 'DIN',
-    maxPixels: 3e8
-  });
   
-  if (is_display) {
-    // Make a display image for the vectors, add it to the map.
-    var display = ee.Image(0).updateMask(0).paint(vector, '000000', 2);
-    Map.addLayer(display, {palette: '000000'}, layerName, false);
-  }
-  if (is_export) {
-    // Export the FeatureCollection to a KML file.
-    Export.table.toDrive({
-      collection: vector,
-      description: fileName,
-      folder:exportFolder,
-      fileNamePrefix: fileName,
-      fileFormat: 'GeoJSON'
-    });
+// Get a real image to ensure the geopatial coordinates are valid
+var img = ee.Image("COPERNICUS/S2/20200909T005709_20200909T005710_T54LXP");
+// Make sure there is nothing to generate a vector from.
+var emptyImg = img.multiply(0);
+
+// Convert the image to vectors.
+var vector = emptyImg.reduceToVectors({
+  geometry: geometry,
+  crs: emptyImg.projection(),
+  scale: 100,
+  geometryType: 'polygon',
+  eightConnected: false,
+  labelProperty: 'DIN',
+  maxPixels: 3e8
+});
+
+// Export the FeatureCollection to a KML file.
+Export.table.toDrive({
+  collection: vector,
+  description: 'World_AIMS_Marine-sat-imagery_Debug_empty-poly',
+  folder: 'World_AIMS_Marine-sat-imagery/debug',
+  fileNamePrefix: 'World_AIMS_Marine-sat-imagery_Debug_empty-poly',
+  fileFormat: 'GeoJSON'
+});
