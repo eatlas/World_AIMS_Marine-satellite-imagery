f7588db8ac7a5557ccb605c917d22d2e136b32c6
eric.lawrey <eric.lawrey@gmail.com>
2022-09-07 00:14:46 +0000

    Updated src/gee/s2-composites/Red-Sea-Gulf-Aden.js (added 1 line, deleted 1 line).

--- src/gee/s2-composites/Red-Sea-Gulf-Aden.js
+++ src/gee/s2-composites/Red-Sea-Gulf-Aden.js
@@ -4,7 +4,7 @@ var s2Utils = require('users/ericlawrey/World_AIMS_Marine-satellite-imagery:src/
 var REF1_OPTIONS = {
   colourGrades: ['DeepFalse','TrueColour','ReefTop','Shallow','Slope'],
   exportScale: [10, 10, 10, 10, 30],
-  exportBasename: 'World_AIMS_Marine-satellite-imagery_R1',
+  exportBasename: 'World_AIMS_Marine-satellite-imagery_S2_R1',
   exportFolder: 'EarthEngine/World_AIMS_Marine-satellite-imagery/Red-Sea-Gulf-Aden',
   
   applySunglintCorrection: true,
