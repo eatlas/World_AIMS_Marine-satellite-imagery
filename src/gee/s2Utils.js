312b505ade5061bdb3e4309734934db700c49306
eric.lawrey <eric.lawrey@gmail.com>
2022-09-27 02:10:57 +0000

    Updated src/gee/s2Utils.js (added 1 line, deleted 1 line).

--- src/gee/s2Utils.js
+++ src/gee/s2Utils.js
@@ -2011,7 +2011,7 @@ exports.createSelectSentinel2ImagesApp = function(tileID, startDate, endDate, cl
     // expensive and significantly slows down the calculation of the images.
     var visParams = {'min': 0, 'max': 1, 'gamma': 1};
     var composite = imagesFiltered
-      .map(exports.removeSunGlint)
+      //.map(exports.removeSunGlint)
       .reduce(ee.Reducer.percentile([50],["p50"]))
       //.reduce(ee.Reducer.first())
       .rename(['B1','B2','B3','B4','B5','B6','B7','B8',
