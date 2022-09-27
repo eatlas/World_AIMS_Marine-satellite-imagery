65b00464ae1d6c5da3237e97907a2cb4bde67077
eric.lawrey <eric.lawrey@gmail.com>
2022-09-27 11:21:50 +0000

    Updated src/gee/s2-composites/GBR-Torres-Strait.js (added 2 lines, deleted 2 lines).

--- src/gee/s2-composites/GBR-Torres-Strait.js
+++ src/gee/s2-composites/GBR-Torres-Strait.js
@@ -16,7 +16,7 @@ var REF1_OPTIONS = {
 };
 
 // High SunGlint
-var REF1_OPTIONS_HSG = REF1_OPTIONS;
+var REF1_OPTIONS_HSG = REF2_OPTIONS;
 REF1_OPTIONS_HSG.sunglintCorrectionLevel = 2;
 
 
@@ -31,7 +31,7 @@ var REF2_OPTIONS = {
 };
 
 // High SunGlint
-var REF2_OPTIONS_HSG = REF1_OPTIONS;
+var REF2_OPTIONS_HSG = REF2_OPTIONS;
 REF2_OPTIONS_HSG.sunglintCorrectionLevel = 2;
 
 // ===============================================================
