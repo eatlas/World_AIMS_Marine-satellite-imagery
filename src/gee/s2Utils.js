f1069c154ad597e366a7ea28733d81b28c184506
eric.lawrey <eric.lawrey@gmail.com>
2022-09-28 11:28:27 +0000

    Updated src/gee/s2Utils.js (added 1 line, deleted 1 line).

--- src/gee/s2Utils.js
+++ src/gee/s2Utils.js
@@ -1423,7 +1423,7 @@ exports.bake_s2_colour_grading = function(img, colourGradeStyle, processCloudMas
     
     // Version 1.4.0 - Adjustment for change in sunglint correction
     B3contrast = exports.contrastEnhance(scaled_img.select('B3'),0.031,0.235, 2.3);
-    B2contrast = exports.contrastEnhance(scaled_img.select('B2'),0.068,0.235, 2.7);
+    B2contrast = exports.contrastEnhance(scaled_img.select('B2'),0.068,0.235, 2.6);
     B1contrast = exports.contrastEnhance(scaled_img.select('B1'),0.1075,0.237, 2.7); 
     compositeContrast = ee.Image.rgb(B3contrast, B2contrast, B1contrast);
 
