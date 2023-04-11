98f4f971f378ed5db705e764e6edc55baaa3416e
eric.lawrey <eric.lawrey@gmail.com>
2023-04-11 11:06:43 +0000

    Updated src/01-gee/sentinel3/apps/01-cloudy-time-series.js (added 2 lines, deleted 2 lines).

--- src/01-gee/sentinel3/apps/01-cloudy-time-series.js
+++ src/01-gee/sentinel3/apps/01-cloudy-time-series.js
@@ -9,12 +9,12 @@ var region =
 //    .select('B[1-7]');
     
 var s3 = ee.ImageCollection('COPERNICUS/S3/OLCI')
-    .filterDate('2018-01-01', '2018-04-04')
+    .filterDate('2017-01-01', '2018-04-04')
     .select('Oa0[3-5]_radiance');
 
 // Create an image time series chart.
 var chart = ui.Chart.image.series({
-  imageCollection: s3,
+  imageCollection: s3.select('Oa03_radiance'),
   region: region,
   reducer: ee.Reducer.mean(),
   scale: 60000
