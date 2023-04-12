2c5d0a1a8627911b73fd38fa00783c9feb3d0779
eric.lawrey <eric.lawrey@gmail.com>
2023-04-12 05:20:16 +0000

    Updated src/01-gee/sentinel3/apps/01-cloudy-time-series.js (added 1 line, deleted 1 line).

--- src/01-gee/sentinel3/apps/01-cloudy-time-series.js
+++ src/01-gee/sentinel3/apps/01-cloudy-time-series.js
@@ -141,7 +141,7 @@ Map.add(buttonPanel);
 
 // Function to step through the chart
 function stepChart(step) {
-  print(chart.getDataTable());
+  print(chart.data());
   var currentSelection = chart.getDataTable()[0];
 
   // If there is no current selection, use the first point
