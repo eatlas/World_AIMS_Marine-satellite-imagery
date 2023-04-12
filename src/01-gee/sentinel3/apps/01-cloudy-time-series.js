cd6340c0c8e33f8856de58ede61c57a1a237cc0d
eric.lawrey <eric.lawrey@gmail.com>
2023-04-12 00:25:19 +0000

    Updated src/01-gee/sentinel3/apps/01-cloudy-time-series.js (added 3 lines, deleted 3 lines).

--- src/01-gee/sentinel3/apps/01-cloudy-time-series.js
+++ src/01-gee/sentinel3/apps/01-cloudy-time-series.js
@@ -112,10 +112,10 @@ function handleChartClick(chart) {
     print(image); 
     var s3Layer = ui.Map.Layer(image, {
       gamma: 1.3,
-      min: 30, // a03 40
-      max: 70, // a03 85
+      min: 30, // a03 40 30
+      max: 70, // a03 85 70
       //bands: ['Oa05_radiance', 'Oa04_radiance', 'Oa03_radiance']
-      bands: ['Oa04_radiance']
+      bands: ['Oa05_radiance']
     }, 'Sentinel 3');
     Map.layers().reset([s3Layer, sfLayer]);
 
