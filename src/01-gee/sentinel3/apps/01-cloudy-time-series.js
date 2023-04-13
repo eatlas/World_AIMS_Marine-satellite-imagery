e05f0c02696df858f46ad3989106a7d650bbee1f
eric.lawrey <eric.lawrey@gmail.com>
2023-04-13 04:51:26 +0000

    Updated src/01-gee/sentinel3/apps/01-cloudy-time-series.js (added 1 line, deleted 1 line).

--- src/01-gee/sentinel3/apps/01-cloudy-time-series.js
+++ src/01-gee/sentinel3/apps/01-cloudy-time-series.js
@@ -21,7 +21,7 @@ var geometry = /* color: #d63000 */ee.Geometry.MultiPoint(
 // demonstrate interactive charts.
     
 var s3 = ee.ImageCollection('COPERNICUS/S3/OLCI')
-    .filterDate('2017-01-01', '2017-01-30');
+    .filterDate('2017-06-01', '2017-06-30');
     //.select('Oa0[4-14]_radiance');
     
 
