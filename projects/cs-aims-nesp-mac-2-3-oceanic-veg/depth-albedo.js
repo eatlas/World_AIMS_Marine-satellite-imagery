0bc81a98bdd9633ffbfe06d8147572db7009c48b
eric.lawrey <eric.lawrey@gmail.com>
2024-03-07 03:34:18 +0000

    Updated projects/cs-aims-nesp-mac-2-3-oceanic-veg/depth-albedo.js (added 17 lines, deleted 1 line).

--- projects/cs-aims-nesp-mac-2-3-oceanic-veg/depth-albedo.js
+++ projects/cs-aims-nesp-mac-2-3-oceanic-veg/depth-albedo.js
@@ -134,4 +134,20 @@ s2Utils.s2_composite_display_and_export(
     //"COPERNICUS/S2_HARMONIZED/20210721T001109_20210721T001111_T56KLF", // Pretty good
     //"COPERNICUS/S2_HARMONIZED/20230313T001119_20230313T001113_T56KLF" // Pretty good
   ],
-  false, true, REF1_OPTIONS);
+  false, false, REF1_OPTIONS);
+  
+// ======== Ashmore Reef (Coral Sea) - Far North =========
+// Searched 73 out of 73 
+// 2016 - 2024-03-01
+s2Utils.s2_composite_display_and_export(
+  [
+    "COPERNICUS/S2_HARMONIZED/20160823T004902_20160823T021200_T54LZP", // clouds, but shows some vegetation structure
+    //"COPERNICUS/S2_HARMONIZED/20180813T004711_20180813T004705_T54LZP", // Clear top half, clouds in bottom. High CDOM
+    "COPERNICUS/S2_HARMONIZED/20200414T004711_20200414T004705_T54LZP", // Cloud free, vegetation not super clear due to sunglint
+    //"COPERNICUS/S2_HARMONIZED/20200822T004711_20200822T004712_T54LZP", // Low cloud, but sunglint reduces depth visbility
+    "COPERNICUS/S2_HARMONIZED/20210603T004709_20210603T004707_T54LZP", // Some cloud patches. Vegetation boundaries visible in parts. 9/10
+    "COPERNICUS/S2_HARMONIZED/20210723T004709_20210723T004708_T54LZP", // Cloud free, low glint, vegetation not very visible
+    "COPERNICUS/S2_HARMONIZED/20210802T004709_20210802T004707_T54LZP", // Some cloud, vegetation not very visible
+    "COPERNICUS/S2_HARMONIZED/20220529T004709_20220529T004706_T54LZP", // very low cloud, moderately clear 6/10
+  ],
+  false, true,REF1_OPTIONS);
