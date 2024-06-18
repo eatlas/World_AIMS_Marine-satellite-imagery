0caafaf6e2f59a45cd3bf12d573171bfc4dcc885
eric.lawrey <eric.lawrey@gmail.com>
2024-06-18 00:01:21 +0000

    Updated projects/CapBunk_AIMS_Regional-map/create-map-sentinel-2-imagery.js (added 5 lines, deleted 2 lines).

--- projects/CapBunk_AIMS_Regional-map/create-map-sentinel-2-imagery.js
+++ projects/CapBunk_AIMS_Regional-map/create-map-sentinel-2-imagery.js
@@ -3,8 +3,11 @@ var s2Utils = require('users/ericlawrey/World_AIMS_Marine-satellite-imagery:src/
 // Primary imagery
 var REF1_OPTIONS = {
   // Preview
-  colourGrades: ['DeepFalse', 'TrueColour'],
-  exportScale: [10, 10],
+  //colourGrades: ['DeepFalse', 'TrueColour'],
+  //exportScale: [10, 10],
+  colourGrades: ['B12'],
+  exportScale: [20],
+  
   exportBasename: 'Wld_AIMS_Marine-sat-img_S2_R1',
   exportFolder: 'EarthEngine/Wld_AIMS_Marine-sat-img/GBR-TS',
   
